087f5916a30aa1ab4ac7d758d9358f5a0286f91e
Chyisiang <chyisiang8@gmail.com>
2023-09-21 19:51:51 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>hi</h1>
+    </>
+  )
+}
+
+export default App
