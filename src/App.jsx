0c853029ea510f9dc8b92360fb221e39ee538dda
Chyisiang <chyisiang8@gmail.com>
2023-09-21 21:41:49 +0800

    basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import FlashcardPage from './components/FlashcardPage'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <FlashcardPage />
--- src/components/FlashcardList.jsx
+++ src/components/FlashcardList.jsx
@@ -4,6 +4,12 @@ import Flashcard from './Flashcard';
 function FlashcardList() {
     const flashcards = [
         { id: 1, question: 'test question', answer: 'test answer' },
+        { id: 2, question: 'test question', answer: 'test answer' },
+        { id: 3, question: 'test VERY LOOOOOOOOOOOOOOOOONGGGGGGGGGGGGGGGGGGGGGGGGG QUESTIONNNNNNNNNNN', answer: 'test LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONGGGGGGGGGGGGGGGGGGGGGGanswer' },
+        { id: 4, question: 'test question', answer: 'test answer' },
+        { id: 5, question: 'test question', answer: 'test answer' },
+        { id: 6, question: 'test question', answer: 'test answer' },
+        { id: 7, question: 'test question', answer: 'test answer' },
     ];
 
     return (
