3145c2c85cac6296445883146312eeb8d1e9a9e9
Chyisiang <chyisiang8@gmail.com>
2023-09-23 16:07:57 +0800

    edit flashcards

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,18 +1,41 @@
 import React, { useState } from 'react';
 
-function Flashcard({ flashcard }) {
+function Flashcard({ flashcard, onEdit, onDelete }) {
     const [isFlipped, setIsFlipped] = useState(false);
+    const [editedFlashcard, setEditedFlashcard] = useState({ ...flashcard });
 
     const handleFlip = () => {
         setIsFlipped(!isFlipped);
     };
 
+    const handleEdit = () => {
+        onEdit(editedFlashcard);
+        setIsFlipped(false);
+    };
+
     return (
         <div className={`flashcard ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
             <div className="flashcard-content">
-                <div className="flashcard-text">
-                    {isFlipped ? flashcard.answer : flashcard.question}
-                </div>
+                {isFlipped ? (
+                    <div className="flashcard-text">
+                        {flashcard.answer}
+                    </div>
+                ) : (
+                    <div>
+                        <textarea
+                            rows="2"
+                            value={editedFlashcard.question}
+                            onChange={(e) => setEditedFlashcard({ ...editedFlashcard, question: e.target.value })}
+                        />
+                        <textarea
+                            rows="2"
+                            value={editedFlashcard.answer}
+                            onChange={(e) => setEditedFlashcard({ ...editedFlashcard, answer: e.target.value })}
+                        />
+                        <button onClick={handleEdit}>Save</button>
+                        <button onClick={onDelete}>Delete</button>
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/components/FlashcardList.jsx
+++ src/components/FlashcardList.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Flashcard from './Flashcard';
 
 function FlashcardList() {
-    const flashcards = [
+    const [flashcards, setFlashcards] = useState([
         { id: 1, question: 'test question', answer: 'test answer' },
         { id: 2, question: 'test question', answer: 'test answer' },
         { id: 3, question: 'test VERY LOOOOOOOOOOOOOOOOONGGGGGGGGGGGGGGGGGGGGGGGGG QUESTIONNNNNNNNNNN', answer: 'test LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONGGGGGGGGGGGGGGGGGGGGGGanswer' },
@@ -10,12 +10,38 @@ function FlashcardList() {
         { id: 5, question: 'test question', answer: 'test answer' },
         { id: 6, question: 'test question', answer: 'test answer' },
         { id: 7, question: 'test question', answer: 'test answer' },
-    ];
+    ]);
+
+    const addFlashcard = () => {
+        const newId = Math.max(...flashcards.map(flashcard => flashcard.id)) + 1;
+        const newFlashcard = { id: newId, question: '', answer: '' };
+        setFlashcards([...flashcards, newFlashcard]);
+    };
+
+    const editFlashcard = (id, updatedFlashcard) => {
+        const updatedFlashcards = flashcards.map(flashcard =>
+            flashcard.id === id ? updatedFlashcard : flashcard
+        );
+        setFlashcards(updatedFlashcards);
+    };
+
+    const deleteFlashcard = (id) => {
+        const updatedFlashcards = flashcards.filter(flashcard =>
+            flashcard.id !== id
+        );
+        setFlashcards(updatedFlashcards);
+    };
 
     return (
         <div className="flashcard-list">
+            <button onClick={addFlashcard}>Add Flashcard</button>
             {flashcards.map(flashcard => (
-                <Flashcard key={flashcard.id} flashcard={flashcard} />
+                <Flashcard
+                    key={flashcard.id}
+                    flashcard={flashcard}
+                    onEdit={updatedFlashcard => editFlashcard(flashcard.id, updatedFlashcard)}
+                    onDelete={() => deleteFlashcard(flashcard.id)}
+                />
             ))}
         </div>
     );
